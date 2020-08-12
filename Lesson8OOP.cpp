adca7c81fdce279947db00c892325409af5c035b
Paul Standley <paulstandley1972@gmail.com>
2020-08-12 18:26:32 +0100

    Initializer list order

--- Lesson8OOP.cpp
+++ Lesson8OOP.cpp
@@ -52,6 +52,8 @@ void class_cpp_oop_85a()
      Assigning values to const or reference member variables 
      in the body of the constructor is clearly not possible in some cases.
 
+     ////////////////////////////////////////////////////////////////////////////
+
      Member initializer lists
 
     To solve this problem, C++ provides a method for initializing class 
@@ -173,6 +175,118 @@ void class_cpp_oop_85a()
         {
         }
     };
+
+    /////////////////////////////////////////////////////////////////////////////
+
+    Initializing member variables that are classes
+
+    A member initialization list can also be used to initialize 
+    members that are classes.
+
+    #include <iostream>
+
+    class A
+    {
+    public:
+        A(int x) { std::cout << "A " << x << '\n'; }
+    };
+
+    class B
+    {
+    private:
+        A m_a;
+    public:
+        B(int y)
+             : m_a{ y-1 } // call A(int) constructor to initialize member m_a
+        {
+            std::cout << "B " << y << '\n';
+        }
+    };
+
+    int main()
+    {
+        B b{ 5 };
+        return 0;
+    }
+
+    When variable b is constructed, 
+    the B(int) constructor is called with value 5. 
+    Before the body of the constructor executes,
+    m_a is initialized, calling the A(int) constructor with value 4. 
+    This prints “A 4”. 
+    Then control returns back to the B constructor, 
+    and the body of the B constructor executes,
+    printing “B 5”.
+
+    /////////////////////////////////////////////////////////////////////
+
+    Formatting your initializer lists
+
+    C++ gives you a lot of flexibility in how to format your initializer lists,
+    and it’s really up to you how you’d like to proceed.
+    But here are some recommendations:
+
+    If the initializer list fits on the same line as the function name,
+    then it’s fine to put everything on one line
+
+    If the initializer list doesn’t fit on the same line as the function name,
+    then it should go indented on the next line.
+
+    If all of the initializers don’t fit on a single line 
+    (or the initializers are non-trivial),
+    then you can space them out, one per line:
+
+    class Something
+    {
+    private:
+        int m_value1;
+        double m_value2;
+        char m_value3;
+        float m_value4;
+
+    public:
+        Something(int value1, double value2, char value3='c', float value4=34.6f) // this line already has a lot of stuff on it
+            : m_value1{ value1 }, // one per line, commas at end of each line
+            m_value2{ value2 },
+            m_value3{ value3 },
+            m_value4{ value4 }
+        {
+        }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    Initializer list order
+
+    Perhaps surprisingly, 
+    variables in the initializer list are not initialized in the
+    order that they are specified in the initializer list. 
+    Instead, they are initialized in the order in which they are
+    declared in the class.
+
+    For best results, the following recommendations should be observed:
+
+    1) Don’t initialize member variables in such a way that they are dependent
+    upon other member variables being initialized first 
+    (in other words, ensure your member variables will properly initialize
+    even if the initialization ordering is different).
+
+    2) Initialize variables in the initializer list in the same order in which they
+    are declared in your class. 
+    This isn’t strictly required so long as the prior
+    recommendation has been followed, 
+    but your compiler may give you a warning if you don’t 
+    do so and you have all warnings turned on.
+
+    Summary
+
+    Member initializer lists allow us to initialize our members rather 
+    than assign values to them. 
+    This is the only way to initialize members that require values upon initialization,
+    such as const or reference members, 
+    and it can be more performant than assigning values in the body of the constructor.
+    Member initializer lists work both with fundamental types and members that are 
+    classes themselves.
 
     */
 }
