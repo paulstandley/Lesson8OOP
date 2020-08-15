5df7d9b5b98b2b0e239193963f5e41575c130cfe
Paul Standley <paulstandley1972@gmail.com>
2020-08-15 10:57:31 +0100

    Defining member functions outside the class definition

--- Lesson8OOP.cpp
+++ Lesson8OOP.cpp
@@ -14,10 +14,104 @@
 #include <iostream>
 
 
+class Datemfocd
+{
+private:
+    int m_year;
+    int m_month;
+    int m_day;
+
+public:
+    Datemfocd(int year, int month, int day);
+
+    void SetDate(int year, int month, int day);
+
+    int getYear() { return m_year; }
+    int getMonth() { return m_month; }
+    int getDay() { return m_day; }
+};
+
+// Date constructor
+Datemfocd::Datemfocd(int year, int month, int day)
+{
+    SetDate(year, month, day);
+}
+
+// Date member function
+void Datemfocd::SetDate(int year, int month, int day)
+{
+    m_month = month;
+    m_day = day;
+    m_year = year;
+}
+
+class Calcmfocd
+{
+private:
+    int m_value = 0;
+
+public:
+    Calcmfocd(int value = 0);
+
+    Calcmfocd& add(int value);
+    Calcmfocd& sub(int value);
+    Calcmfocd& mult(int value);
+
+    int getValue() { return m_value; }
+};
+
+Calcmfocd::Calcmfocd(int value) : m_value(value)
+{
+}
+
+Calcmfocd& Calcmfocd::add(int value)
+{
+    m_value += value;
+    return *this;
+}
+
+Calcmfocd& Calcmfocd::sub(int value)
+{
+    m_value -= value;
+    return *this;
+}
+
+Calcmfocd& Calcmfocd::mult(int value)
+{
+    m_value *= value;
+    return *this;
+}
 
 void class_cpp_oop_89()
 {
+    /*Defining member functions outside the class definition
 
+    All of the classes that we have written so far have been 
+    simple enough that we have been able to implement the member 
+    functions directly inside the class definition itself.
+
+    However, as classes get longer and more complicated, 
+    having all the member function definitions inside the class 
+    can make the class harder to manage and work with. 
+    
+    Using an already-written class only requires understanding 
+    its public interface (the public member functions), 
+    not how the class works underneath the hood.
+    The member function implementation details just get in the way.
+
+    Fortunately,
+    C++ provides a way to separate the “declaration” portion of the class
+    from the “implementation” portion.
+    This is done by defining the class member functions
+    outside of the class definition. 
+    To do so, simply define the member functions 
+    of the class as if they were normal functions, 
+    but prefix the class name to the function using the 
+    scope resolution operator (::) (same as for a namespace).
+
+Here is our Date class with the Date constructor and setDate() function defined outside of the class definition. Note that the prototypes for these functions still exist inside the class definition, but the actual implementation has been moved outside:
+
+    */
 }
 
 
