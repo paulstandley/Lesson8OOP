161fb6f9f049a6f089d41023d7205da52652fa53
Paul Standley <paulstandley1972@gmail.com>
2020-08-19 18:53:56 +0100

    nested types in classes

--- Lesson8OOP.cpp
+++ Lesson8OOP.cpp
@@ -136,10 +136,27 @@ void class_cpp_oop_815()
 	*/
 }
 
+void timing_your_code()
+{
+	/*When writing your code, 
+	sometimes you’ll run across cases where you’re not sure whether one method 
+	or another will be more performant. So how do you tell?
+
+	One easy way is to time your code to see how long it takes to run. 
+	C++11 comes with some functionality in the chrono library to do just that.
+	
+	However, 
+	using the chrono library is a bit arcane. 
+	The good news is that we can easily encapsulate all the timing functionality 
+	we need into a class that we can then use in our own programs.
+	
+	*/
+}
+
 
 int main()
 {
-    class_cpp_oop_815();
+    timing_your_code();
 
 
     return 0;
