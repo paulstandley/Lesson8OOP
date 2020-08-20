74fc16562aaf0b414e098104c8902b8a6b0c4a25
Paul Standley <paulstandley1972@gmail.com>
2020-08-20 21:02:36 +0100

    timing_your_code

--- Lesson84.cpp
+++ Lesson84.cpp
@@ -11,6 +11,8 @@
 #include <iostream>
 #include "Vector3d.h" // for creating Vector3d object
 #include "Point3d.h" // for creating Point3d object
+#include <chrono> // for std::chrono functions
+#include <algorithm> // for std::sort
 
 
 class Storagecf
@@ -527,4 +529,125 @@ void class_cpp_oop_814()
 
     */
 }
+
+enum FruitType11
+{
+    APPLE,
+    BANANA,
+    CHERRY
+};
+
+class Fruit11
+{
+private:
+    FruitType11 m_type;
+    int m_percentageEaten = 0;
+
+public:
+
+    Fruit11(FruitType11 type) :
+        m_type(type) {}
+
+    FruitType11 getType11() { return m_type; }
+    int getPercentageEaten() { return m_percentageEaten; }
+};
+
+class Fruit12
+{
+public:
+    // Note: we've moved FruitType inside the class, under the public access specifier
+    enum FruitType12
+    {
+        APPLE,
+        BANANA,
+        CHERRY
+    };
+
+private:
+    FruitType12 m_type;
+    int m_percentageEaten = 0;
+
+public:
+
+
+    Fruit12(FruitType12 type) :
+        m_type(type)
+    {
+    }
+
+    FruitType12 getType12() { return m_type; }
+    int getPercentageEaten() { return m_percentageEaten; }
+};
+
+void class_cpp_oop_815()
+{
+    Fruit11 apple(APPLE);
+
+    if (apple.getType11() == APPLE)
+        std::cout << "I am an apple";
+    else
+        std::cout << "I am not an apple";
+    /*There’s nothing wrong with this program.
+    But because enum FruitType is meant to be used in conjunction with the Fruit class,
+    it’s a little weird to have it exist independently from the class itself.
+
+    Nesting types
+
+    Unlike functions, which can’t be nested inside each other, in C++,
+    types can be defined (nested) inside of a class.
+    To do this, you simply define the type inside the class,
+    under the appropriate access specifier.
+
+    Here’s the same program as above, with FruitType defined inside the class
+
+    */
+
+    // Note: we access the FruitType via Fruit now
+    Fruit12 apple12(Fruit12::APPLE);
+
+    if (apple12.getType12() == Fruit12::APPLE)
+        std::cout << "I am an apple";
+    else
+        std::cout << "I am not an apple";
+
+    /*First, note that FruitType is now defined inside the class.
+    Second, note that we’ve defined it under the public access specifier,
+    so the type definition can be accessed from outside the class.
+
+    Classes essentially act as a namespace for any nested types.
+    In the prior example, we were able to access enumerator APPLE directly,
+    because the APPLE enumerator was placed into the global scope
+    (we could have prevented this by using an enum class instead of an enum,
+    in which case we’d have accessed APPLE via FruitType::APPLE instead).
+
+    Now, because FruitType is considered to be part of the class,
+    we access the APPLE enumerator by prefixing it with the class name:
+    Fruit::APPLE.
+
+    Note that because enum classes also act like namespaces,
+    if we’d nested FruitType inside Fruit as an enum class instead of an enum,
+    we’d access the APPLE enumerator via Fruit::FruitType::APPLE.
+
+    Other types can be nested too
+
+    Although enumerations are probably the most common type that is nested
+    inside a class, C++ will let you define other types within a class,
+    such as typedefs, type aliases, and even other classes!
+
+    Like any normal member of a class,
+    nested classes have the same access to members of the enclosing class
+    that the enclosing class does.
+    However, the nested class does not have any special access
+    to the “this” pointer of the enclosing class.
+
+    One other limitation of nested types -- they can’t be forward declared.
+    This limitation may be lifted in a future version of C++.
+
+    Defining nested classes isn’t very common,
+    but the C++ standard library does do so in some cases,
+    such as with iterator classes.
+
+    */
+}
+
 
--- Lesson8OOP.cpp
+++ Lesson8OOP.cpp
@@ -14,126 +14,137 @@
 #include <iostream>
 #include "Vector3d.h" // for creating Vector3d object
 #include "Point3d.h" // for creating Point3d object
+#include <chrono> // for std::chrono functions
+#include <algorithm> // for std::sort
 
 
-enum FruitType11
-{
-	APPLE,
-	BANANA,
-	CHERRY
-};
-
-class Fruit11
+class Timer1
 {
 private:
-	FruitType11 m_type;
-	int m_percentageEaten = 0;
+	// Type aliases to make accessing nested type easier
+	using clock_t = std::chrono::high_resolution_clock;
+	using second_t = std::chrono::duration<double, std::ratio<1> >;
+
+	std::chrono::time_point<clock_t> m_beg;
 
 public:
-
-	Fruit11(FruitType11 type) :
-		m_type(type){}
-
-	FruitType11 getType11() { return m_type; }
-	int getPercentageEaten() { return m_percentageEaten; }
-};
-
-class Fruit12
-{
-public:
-	// Note: we've moved FruitType inside the class, under the public access specifier
-	enum FruitType12
-	{
-		APPLE,
-		BANANA,
-		CHERRY
-	};
-
-private:
-	FruitType12 m_type;
-	int m_percentageEaten = 0;
-
-public:
-
-
-	Fruit12(FruitType12 type) :
-		m_type(type)
+	Timer1() : m_beg(clock_t::now())
 	{
 	}
 
-	FruitType12 getType12() { return m_type; }
-	int getPercentageEaten() { return m_percentageEaten; }
+	void reset1()
+	{
+		m_beg = clock_t::now();
+	}
+
+	double elapsed1() const
+	{
+		return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+	}
 };
 
-void class_cpp_oop_815()
+const int g_arrayElements = 10000;
+
+class Timer2
 {
-	Fruit11 apple(APPLE);
+private:
+	// Type aliases to make accessing nested type easier
+	using clock_t = std::chrono::high_resolution_clock;
+	using second_t = std::chrono::duration<double, std::ratio<1> >;
 
-	if (apple.getType11() == APPLE)
-		std::cout << "I am an apple";
-	else
-		std::cout << "I am not an apple";
-	/*There’s nothing wrong with this program. 
-	But because enum FruitType is meant to be used in conjunction with the Fruit class,
-	it’s a little weird to have it exist independently from the class itself.
+	std::chrono::time_point<clock_t> m_beg;
 
-	Nesting types
+public:
+	Timer2() : m_beg(clock_t::now())
+	{
+	}
 
-	Unlike functions, which can’t be nested inside each other, in C++,
-	types can be defined (nested) inside of a class.
-	To do this, you simply define the type inside the class,
-	under the appropriate access specifier.
+	void reset2()
+	{
+		m_beg = clock_t::now();
+	}
 
-	Here’s the same program as above, with FruitType defined inside the class
-	
-	*/
+	double elapsed2() const
+	{
+		return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+	}
+};
 
-	// Note: we access the FruitType via Fruit now
-	Fruit12 apple12(Fruit12::APPLE);
+void sortArray2(std::array<int, g_arrayElements>& array)
+{
 
-	if (apple12.getType12() == Fruit12::APPLE)
-		std::cout << "I am an apple";
-	else
-		std::cout << "I am not an apple";
+	// Step through each element of the array
+	// (except the last one, which will already be sorted by the time we get there)
+	for (int startIndex = 0; startIndex < g_arrayElements - 1; ++startIndex)
+	{
+		// smallestIndex is the index of the smallest element we’ve encountered this iteration
+		// Start by assuming the smallest element is the first element of this iteration
+		int smallestIndex = startIndex;
 
-	/*First, note that FruitType is now defined inside the class. 
-	Second, note that we’ve defined it under the public access specifier, 
-	so the type definition can be accessed from outside the class.
+		// Then look for a smaller element in the rest of the array
+		for (int currentIndex = startIndex + 1; currentIndex < g_arrayElements; ++currentIndex)
+		{
+			// If we've found an element that is smaller than our previously found smallest
+			if (array[currentIndex] < array[smallestIndex])
+				// then keep track of it
+				smallestIndex = currentIndex;
+		}
 
-	Classes essentially act as a namespace for any nested types. 
-	In the prior example, we were able to access enumerator APPLE directly, 
-	because the APPLE enumerator was placed into the global scope 
-	(we could have prevented this by using an enum class instead of an enum,
-	in which case we’d have accessed APPLE via FruitType::APPLE instead). 
-	
-	Now, because FruitType is considered to be part of the class, 
-	we access the APPLE enumerator by prefixing it with the class name: 
-	Fruit::APPLE.
+		// smallestIndex is now the smallest element in the remaining array
+		// swap our start element with our smallest element (this sorts it into the correct place)
+		std::swap(array[startIndex], array[smallestIndex]);
+	}
+}
 
-	Note that because enum classes also act like namespaces,
-	if we’d nested FruitType inside Fruit as an enum class instead of an enum,
-	we’d access the APPLE enumerator via Fruit::FruitType::APPLE.
+class Timer3
+{
+private:
+	// Type aliases to make accessing nested type easier
+	using clock_t = std::chrono::high_resolution_clock;
+	using second_t = std::chrono::duration<double, std::ratio<1> >;
 
-	Other types can be nested too
+	std::chrono::time_point<clock_t> m_beg;
 
-	Although enumerations are probably the most common type that is nested
-	inside a class, C++ will let you define other types within a class,
-	such as typedefs, type aliases, and even other classes!
+public:
+	Timer3() : m_beg(clock_t::now())
+	{
+	}
 
-	Like any normal member of a class, 
-	nested classes have the same access to members of the enclosing class 
-	that the enclosing class does.
-	However, the nested class does not have any special access
-	to the “this” pointer of the enclosing class.
+	void reset3()
+	{
+		m_beg = clock_t::now();
+	}
 
-	One other limitation of nested types -- they can’t be forward declared.
-	This limitation may be lifted in a future version of C++.
+	double elapsed3() const
+	{
+		return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+	}
+};
 
-	Defining nested classes isn’t very common,
-	but the C++ standard library does do so in some cases, 
-	such as with iterator classes.
-	
-	*/
+void sortArray3(std::array<int, g_arrayElements>& array)
+{
+
+	// Step through each element of the array
+	// (except the last one, which will already be sorted by the time we get there)
+	for (int startIndex = 0; startIndex < g_arrayElements - 1; ++startIndex)
+	{
+		// smallestIndex is the index of the smallest element we’ve encountered this iteration
+		// Start by assuming the smallest element is the first element of this iteration
+		int smallestIndex = startIndex;
+
+		// Then look for a smaller element in the rest of the array
+		for (int currentIndex = startIndex + 1; currentIndex < g_arrayElements; ++currentIndex)
+		{
+			// If we've found an element that is smaller than our previously found smallest
+			if (array[currentIndex] < array[smallestIndex])
+				// then keep track of it
+				smallestIndex = currentIndex;
+		}
+
+		// smallestIndex is now the smallest element in the remaining array
+		// swap our start element with our smallest element (this sorts it into the correct place)
+		std::swap(array[startIndex], array[smallestIndex]);
+	}
 }
 
 void timing_your_code()
@@ -149,6 +160,93 @@ void timing_your_code()
 	using the chrono library is a bit arcane. 
 	The good news is that we can easily encapsulate all the timing functionality 
 	we need into a class that we can then use in our own programs.
+	
+	*/
+
+	Timer1 t;
+	// Code to time goes here
+	std::cout << "Time elapsed: " << t.elapsed1() << " seconds\n";
+
+	/*That’s it! To use it,
+	we instantiate a Timer object at the top of our main function
+	(or wherever we want to start timing),
+	and then call the elapsed() 
+	member function whenever we want to know how long the program took 
+	to run to that point.
+	
+	*/
+
+	std::array<int, g_arrayElements> array2;
+	for (int i = 0; i < g_arrayElements; ++i)
+		array2[i] = g_arrayElements - i;
+
+	Timer2 t2;
+	sortArray2(array2);
+	std::cout << "Time taken: " << t2.elapsed2() << " seconds\n";
+
+	//////////////////////////////////////////////////////////////
+
+	std::array<int, g_arrayElements> array3;
+	for (int i = 0; i < g_arrayElements; ++i)
+		array3[i] = g_arrayElements - i;
+
+	Timer3 t3;
+	std::sort(array3.begin(), array3.end());
+	std::cout << "Time taken: " << t3.elapsed3() << " seconds\n";
+
+	/*A few caveats about timing
+
+	Timing is straightforward,
+	but your results can be significantly impacted by a number of things, 
+	and it’s important to be aware of what those things are.
+
+	First, 
+	make sure you’re using a release build target, not a debug build target.
+	Debug build targets typically turn optimization off, 
+	and that optimization can have a significant impact on the results.
+	For example, using a debug build target, 
+	running the above std::sort example on the author’s machine took 0.0235 seconds
+	-- 33 times as long!
+
+	Second, 
+	your timing results will be influenced by other things your system 
+	may be doing in the background. 
+	For best results, make sure your system isn’t doing anything CPU or memory 
+	intensive (e.g. playing a game) or hard drive intensive 
+	(e.g. searching for a file or running an antivirus scan).
+
+	Then measure at least 3 times. 
+	If the results are all similar, 
+	take the average. If one or two results are different, 
+	run the program a few more times until you get a better sense 
+	of which ones are outliers.
+	Note that seemingly innocent things, 
+	like web browsers, can temporarily spike your CPU to 100% utilization
+	when the site you have sitting in the background rotates
+	in a new ad banner and has to parse a bunch of javascript. 
+	Running multiple times helps identify whether your initial run may 
+	have been impacted by such an event.
+
+	Third, when doing comparisons between two sets of code, 
+	be wary of what may change between runs that could impact timing. 
+	Your system may have kicked off an antivirus scan in the background, 
+	or maybe you’re streaming music now when you weren’t previously. 
+	Randomization can also impact timing.
+	
+	If we’d sorted an array filled with random numbers, 
+	the results could have been impacted by the randomization.
+	Randomization can still be used, but ensure you use a fixed seed 
+	(e.g. don’t use the system clock)
+	so the randomization is identical each run. 
+	Also, make sure you’re not timing waiting for user input,
+	as how long the user takes to input something should not be part of your 
+	timing considerations.
+
+	Finally, 
+	note that results are only valid for your machine’s architecture,
+	OS, compiler, and system specs.
+	You may get different results on other systems that have different strengths
+	and weaknesses.
 	
 	*/
 }
